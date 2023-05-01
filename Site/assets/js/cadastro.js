655eb0f0e1a33083a148f9a2e899c9bccc2cd0fc
Kauanny Tenorio <kakau24112004@hotmail.com>
2023-05-01 15:37:19 -0300

    Adicionando validação inicial no cadastro

---
+++ Site/assets/js/cadastro.js
@@ -0,0 +1,42 @@
+function cadastrarUsuario(){
+    var nome = ipt_nome.value;
+    var email = ipt_email.value;
+    var username = ipt_username.value;
+    var senha = ipt_senha.value;
+    var confirmacao_senha = ipt_confirmar_senha.value;
+
+    if(nome == "" || email == "" || username == "" || senha == "" || confirmacao_senha == ""){
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'Campo obrigatório vazio.',
+            showConfirmButton: true,
+        });
+    }else if(senha != confirmacao_senha){
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'Senhas não coincidem.',
+            showConfirmButton: true,
+        });
+    }else if(senha.length < 4 || senha.length > 50){
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'A senha deve conter no mínimo 4 e no máximo 50 caracteres.',
+            showConfirmButton: true,
+        });
+    }else{
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Cadastro realizado com sucesso.',
+            showConfirmButton: false,
+            timer: 1500
+        });
+        
+        setTimeout(function() {
+            window.parent.location.href = "../login.html";
+        }, 1700);
+    }
+}
